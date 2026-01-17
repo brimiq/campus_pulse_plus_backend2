cdf60b05f3775be9541bcc09602f43efbef50ee1
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-01-17 15:27:06 +0300

    Add Reaction and AdminResponse class

--- models.py
+++ models.py
@@ -82,4 +82,37 @@ class Comment(db.Model):
     post = db.relationship("Post", back_populates="comments")
 
 
+class Reaction(db.Model):
+    __tablename__ = "reaction"
+
+    id = db.Column(db.Integer, primary_key=True)
+    reaction_type = db.Column(db.String(10), nullable=False)  # like | dislike
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+
+    user = db.relationship("User", back_populates="reactions")
+    post = db.relationship("Post", back_populates="reactions")
+
+    __table_args__ = (
+        db.UniqueConstraint("user_id", "post_id", name="unique_user_post_reaction"),
+    )
+
+
+
+class AdminResponse(db.Model):
+    __tablename__ = "admin_response"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(300), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+    admin_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    post = db.relationship("Post", back_populates="admin_responses")
+    admin = db.relationship("User", back_populates="admin_responses")
+
+
 
